7bafc430e0f24d0c72936c29e5ae96599bf72c96
Najisu <s2322@e-mirim.hs.kr>
2025-04-29 14:26:32 +0900

    #9 음식 아이템 랜덤 생성

---
+++ Assets/Scripts/FoodSpawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] foods;
+
+    private float[] arrPosX = { -6.6f, -5.5f, -4.4f, -3.3f, -2.2f, -1.1f, 0f, 1.1f, 2.2f, 3.3f, 4.4f, 5.5f, 6.6f };
+
+    void Start()
+    {
+        int posX = Random.Range(0, arrPosX.Length);     // 생성 위치를 결정
+        int index = Random.Range(0, foods.Length);      // 종류를 결정
+        SpawnFood(posX, index);
+        
+    }
+
+    void SpawnFood(int posX, int index) {
+        Vector3 spawnPos = new Vector3(arrPosX[posX], 5.5f, transform.position.z);
+        Instantiate(foods[index], spawnPos, Quaternion.identity);
+    }
+}
