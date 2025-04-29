4aeab56d31740d86c961eb0994e163e11d163e1c
Najisu <s2322@e-mirim.hs.kr>
2025-04-29 10:19:48 +0900

    #3 플레이어 움직이기

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float moveSpeed = 0;
+    
     void Start()
     {
 
 
     }
 
-
     void Update()
     {
-
-
+        Vector3 moveTo = new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.position -= moveTo;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += moveTo;
+        }
     }
 }
